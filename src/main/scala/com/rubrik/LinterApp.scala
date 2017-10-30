e6d4e990f7f6251b95958c64790e273eefea84db
Sujeet Gholap <sujeetgholap@gmail.com>
2017-10-30 23:17:05 +0530

    Make all lint issues into warnings
    Summary:
    If the severity of a lint message is "error", then
    arcanist shows the lint message irrespective of whether
    that particular line is touched in the diff or not.
    
    We're not ready for large refactors yet, so in this diff
    we set the severity of all lint messages to warning if
    either the severity is explicitly mentioned as "error",
    or it is not mentioned at all.
    
    Test Plan: manual verification

--- src/main/scala/com/rubrik/LinterApp.scala
+++ src/main/scala/com/rubrik/LinterApp.scala
@@ -5,6 +5,7 @@ import com.rubrik.linter.ExceptionCatchArticleLinter
 import com.rubrik.linter.FunctionCallArgsLinter
 import com.rubrik.linter.FunctionDeclarationLinter
 import com.rubrik.linter.LintResult
+import com.rubrik.linter.LintResult.Severity
 import com.rubrik.linter.Linter
 import com.rubrik.linter.MultilineAssignmentLinter
 import java.nio.file.Paths
@@ -36,7 +37,23 @@ object LinterApp {
     val path = Paths.get(args(0))
     val sourceText = scala.io.Source.fromFile(path.toFile).mkString
     val source = sourceText.parse[Source].get
-    val results: Seq[LintResult] = linters.flatMap(_.lint(source))
+    val results: Seq[LintResult] =
+      linters
+        .flatMap(_.lint(source))
+        // TODO(sujeet): once we're ready for errors to appear
+        // in parts of file that aren't touched in a diff, stop
+        // making everything into a warning.
+        .map {
+          result =>
+            result.copy(
+              severity =
+                result.severity match {
+                  case None => Some(Severity.Warning)
+                  case Some(Severity.Error) => Some(Severity.Warning)
+                  case other => other
+                }
+            )
+        }
     println(Json.prettyPrint(Json.toJson(results)))
   }
 }
