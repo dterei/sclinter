326748d4ea1dbd0829651c8731118933dec89912
Sujeet Gholap <sujeetgholap@gmail.com>
2017-11-07 11:12:55 +0530

    Make function-call-args messages more specific
    Summary:
    This diff breaks down the previous "Each argument
    to `$method` must be on a line of its own and
    appropriately indented." message into more helpful
    and precise messages. See the screenshots in the
    test plan for detailed examples.
    
    Test Plan:
     - Existing unit tests to ensure functionality
       remains the same
     - Screenshots of new, more specific messages:
       - not left-aligned: {F67464}
       - left-aligned but wrong indentation: {F67466}
    
    Reviewers: hao, mudit, sh, dz
    
    Differential Revision: https://phabricator.rubrik.com/D33349

--- src/main/scala/com/rubrik/linter/FunctionCallArgsLinter.scala
+++ src/main/scala/com/rubrik/linter/FunctionCallArgsLinter.scala
@@ -4,6 +4,7 @@ import com.rubrik.linter.util.indent
 import com.rubrik.linter.util.leftAligned
 import com.rubrik.linter.util.sameLine
 import com.rubrik.linter.util.startOnSameLine
+import scala.meta.Term
 import scala.meta.Tree
 import scala.meta.quasiquotes.XtensionQuasiquoteTerm
 
@@ -20,7 +21,7 @@ object FunctionCallArgsLinter extends Linter {
   type IndentAmt = Int
   type IndentSpec = (IndentRef, Method, IndentAmt)
 
-  private def indentSpec(func: Tree): IndentSpec = {
+  private def indentSpec(func: Term): IndentSpec = {
     def spec(obj: Tree, method: Tree): IndentSpec = {
       // Q. Should the args be indented wrt obj, or wrt method?
       // Ans. If they are all on the same line, then obj, else method
@@ -38,38 +39,52 @@ object FunctionCallArgsLinter extends Linter {
     }
   }
 
-  private def invalid(func: Tree, args: Seq[Any]): Boolean = {
+  private def lintResult(funCall: Term.Apply): Option[LintResult] = {
+    val (indentRef, method, indentAmount) = indentSpec(funCall.fun)
 
-    val (indentRef, method, indentAmount) = indentSpec(func)
+    val args = funCall.args
 
-    val fargs = args.asInstanceOf[Seq[Tree]]
-    val Invalid = true
-    val Valid = false
-
-    if (!sameLine(fargs: _*) && !leftAligned(fargs: _*)) {
-      Invalid
-    } else if (fargs.nonEmpty && !startOnSameLine(method, fargs.head)) {
-      indent(fargs.head) != indent(indentRef) + indentAmount
-    } else {
-      Valid
+    def argName(arg: Term): String = {
+      arg match {
+        case assignedArg: Term.Assign => assignedArg.lhs.syntax
+        case _ => arg.syntax
+      }
     }
-  }
 
-  private def lintResult(func: Tree): LintResult = {
-    val (_, method, _) = indentSpec(func)
-    LintResult(
-      message =
-        s"Each argument to `$method` must be on a line of its own " +
-          "and appropriately indented.",
-      code = Some("FUNCTION-CALL-ARGS"),
-      name = Some("Function call arguments"),
-      line = Some(method.pos.startLine + 1),
-      char = Some(method.pos.startColumn + 1))
+    val messageOpt: Option[String] =
+      if (!sameLine(args: _*) && !leftAligned(args: _*)) {
+        Some(
+          s"All arguments to `$method` are not on the same line. " +
+            "When arguments span multiple lines, every argument must be on " +
+            s"a line of its own and left-aligned.")
+      } else if (args.nonEmpty && !startOnSameLine(method, args.head)) {
+        val idealArgIndent = indent(indentRef) + indentAmount
+        val argIndent = indent(args.head)
+        val properlyIndented = argIndent == idealArgIndent
+        if (!properlyIndented) {
+          Some(
+            s"As `$indentRef` is indented by ${indent(indentRef)} spaces, " +
+              s"`${argName(args.head)}` must be indented by $idealArgIndent " +
+              s"spaces, but is actually indented by $argIndent spaces.")
+        } else {
+          None
+        }
+      } else {
+        None
+      }
+
+    messageOpt.map(
+      LintResult(
+        _,
+        code = Some("FUNCTION-CALL-ARGS"),
+        name = Some("Function call arguments"),
+        line = Some(method.pos.startLine + 1),
+        char = Some(method.pos.startColumn + 1)))
   }
 
   override def lint(tree: Tree): Seq[LintResult] = {
-    tree collect {
-      case q"$func(..$args)" if invalid(func, args) => lintResult(func)
-    }
+    tree
+      .collect { case funCall: Term.Apply => lintResult(funCall) }
+      .flatten
   }
 }
